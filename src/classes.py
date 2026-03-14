b65f0c8d96bbb20481e5ea180b4d16af0ff8048f
Andrey Podianov <podyanov82@gmail.com>
2026-03-14 12:47:53 +0500

    task4

--- src/classes.py
+++ src/classes.py
@@ -5,8 +5,71 @@ class Product:
         """Метод для инициализации экземпляра класса Product"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price  # Приватный атрибут цены
         self.quantity = quantity
+
+    # Задание 4
+    @property
+    def price(self):
+        """Геттер для получения цены товара"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        """
+        Сеттер для установки цены товара с проверкой корректности.
+
+        Args:
+            new_price: Новая цена товара
+
+        Returns:
+            None. Если цена некорректна, выводится сообщение об ошибке
+        """
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+    @classmethod
+    def new_product(cls, product_data: dict, existing_products: list = None):
+        """
+        Класс-метод для создания нового продукта из словаря с проверкой на дубликаты.
+
+        Args:
+            product_data: Словарь с данными о продукте
+            existing_products: Список существующих продуктов для проверки дубликатов
+
+        Returns:
+            Объект класса Product (новый или обновленный существующий)
+        """
+        # Проверяем наличие дубликатов, если передан список существующих товаров
+        if existing_products:
+            for existing_product in existing_products:
+                if existing_product.name == product_data['name']:
+                    # Найден дубликат - обновляем существующий товар
+                    print(f"Найден дубликат товара '{product_data['name']}'. Обновляем данные...")
+
+                    # Складываем количество
+                    existing_product.quantity += product_data['quantity']
+
+                    # Выбираем максимальную цену (используем сеттер для проверки)
+                    if product_data['price'] > existing_product.price:
+                        existing_product.price = product_data['price']  # Используем сеттер
+                        print(f"Цена обновлена до {existing_product.price}")
+                    else:
+                        print(f"Цена оставлена прежней: {existing_product.price}")
+
+                    print(f"Новое количество: {existing_product.quantity}")
+                    return existing_product
+
+        # Если дубликатов нет или список не передан, создаем новый продукт
+        print(f"Создаем новый товар '{product_data['name']}'")
+        return cls(
+            name=product_data['name'],
+            description=product_data['description'],
+            price=product_data['price'],
+            quantity=product_data['quantity']
+        )
 
     # # Задание 3
     # @classmethod
@@ -29,51 +92,51 @@ class Product:
     #         quantity=product_data['quantity']
     #     )
 
-    # Доп задание 3
-    @classmethod
-    def new_product(cls, product_data: dict, existing_products: list = None):
-        """
-        Класс-метод для создания нового продукта из словаря с проверкой на дубликаты.
-
-        Args:
-            product_data: Словарь с данными о продукте, содержащий ключи:
-                         'name', 'description', 'price', 'quantity'
-            existing_products: Список существующих продуктов для проверки дубликатов
-
-        Returns:
-            Объект класса Product (новый или обновленный существующий)
-        """
-        # Проверяем наличие дубликатов, если передан список существующих товаров
-        if existing_products:
-            for existing_product in existing_products:
-                if existing_product.name == product_data['name']:
-                    # Найден дубликат - обновляем существующий товар
-                    print(f"Найден дубликат товара '{product_data['name']}'. Обновляем данные...")
-
-                    # Складываем количество
-                    existing_product.quantity += product_data['quantity']
-
-                    # Выбираем максимальную цену
-                    if product_data['price'] > existing_product.price:
-                        existing_product.price = product_data['price']
-                        print(f"Цена обновлена до {existing_product.price}")
-                    else:
-                        print(f"Цена оставлена прежней: {existing_product.price}")
-
-                    # Обновляем описание (можно оставить старое или обновить)
-                    # existing_product.description = product_data['description']
-
-                    print(f"Новое количество: {existing_product.quantity}")
-                    return existing_product
-
-        # Если дубликатов нет или список не передан, создаем новый продукт
-        print(f"Создаем новый товар '{product_data['name']}'")
-        return cls(
-            name=product_data['name'],
-            description=product_data['description'],
-            price=product_data['price'],
-            quantity=product_data['quantity']
-        )
+    # # Доп задание 3
+    # @classmethod
+    # def new_product(cls, product_data: dict, existing_products: list = None):
+    #     """
+    #     Класс-метод для создания нового продукта из словаря с проверкой на дубликаты.
+    #
+    #     Args:
+    #         product_data: Словарь с данными о продукте, содержащий ключи:
+    #                      'name', 'description', 'price', 'quantity'
+    #         existing_products: Список существующих продуктов для проверки дубликатов
+    #
+    #     Returns:
+    #         Объект класса Product (новый или обновленный существующий)
+    #     """
+    #     # Проверяем наличие дубликатов, если передан список существующих товаров
+    #     if existing_products:
+    #         for existing_product in existing_products:
+    #             if existing_product.name == product_data['name']:
+    #                 # Найден дубликат - обновляем существующий товар
+    #                 print(f"Найден дубликат товара '{product_data['name']}'. Обновляем данные...")
+    #
+    #                 # Складываем количество
+    #                 existing_product.quantity += product_data['quantity']
+    #
+    #                 # Выбираем максимальную цену
+    #                 if product_data['price'] > existing_product.price:
+    #                     existing_product.price = product_data['price']
+    #                     print(f"Цена обновлена до {existing_product.price}")
+    #                 else:
+    #                     print(f"Цена оставлена прежней: {existing_product.price}")
+    #
+    #                 # Обновляем описание (можно оставить старое или обновить)
+    #                 # existing_product.description = product_data['description']
+    #
+    #                 print(f"Новое количество: {existing_product.quantity}")
+    #                 return existing_product
+    #
+    #     # Если дубликатов нет или список не передан, создаем новый продукт
+    #     print(f"Создаем новый товар '{product_data['name']}'")
+    #     return cls(
+    #         name=product_data['name'],
+    #         description=product_data['description'],
+    #         price=product_data['price'],
+    #         quantity=product_data['quantity']
+    #     )
 
 
 class Category:
